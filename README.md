11e9d7ada7b601485b49179763558ac203002400
anjalirana99 <anjali.rana021@gmail.com>
2026-09-02 12:53:24 +0530

    setting up routes using declarative routing mode

--- README.md
+++ README.md
@@ -3,3 +3,10 @@ create app using vite
 install npm 
 install daisy UI 
 install react router dom 
+
+
+## Setting up routes 
+APP = path "/" -> Body 
+                    Header 
+                    outlet {children} path = "/login or /signup"
+                    Footer 
--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,21 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom"
+import Body from "./Body"
 import Login from "./Login"
+import Signup from "./Signup"
 
 function App() {
  
-
   return (
     <>
     <BrowserRouter>
       <Routes>
-        <Route path="/login" element={<Login/>}/>
+       <Route path="/" element={<Body/>}>
+          <Route path="/login" element={<Login/>}/>
+          <Route path="/signup" element={<Signup/>}/>
+       </Route>
+          
       </Routes>
     </BrowserRouter>
-    <h1 className="text-3xl font-bold underline">Hello World</h1>
-    <button classNa="btn btn-primary">
-    </button>
     </>
     
   )
